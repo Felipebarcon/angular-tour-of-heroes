f3a0b296c4688f5d0589d583a590d18d1f3efc9f
Felipe Barcon <felipe.barcon@gmail.com>
2023-02-09 01:39:41 +0100

    updating to angular 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
